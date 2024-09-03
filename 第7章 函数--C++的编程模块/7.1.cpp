3aed1bef8f3679e2802270ba40d7c1e58a71c75d
dganshin <3138971910@qq.com>
2024-09-03 21:59:57 +0800

    7.1

--- 第7章 函数--C++的编程模块/7.1.cpp
+++ 第7章 函数--C++的编程模块/7.1.cpp
@@ -1,6 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
+/*******************************************
+ * Author: dganshin, dganshin@qq.com
+ * Algorithm code in C++
+ ******************************************/
+// defining, prototyping, and calling a function
+// 函数的定义, 声明和调用
+#include <iostream>
 
-int main(){
-    
+void simple(); // function prototype
+
+int main() {
+    std::cout << "main() will call the simple function:\n";
+    simple();
+    std::cout << "main() is finished with the simple() function.\n";
+    return 0;
+}
+// function definition
+void simple() {
+    std::cout << "I'm but a simple function.\n";
 }
